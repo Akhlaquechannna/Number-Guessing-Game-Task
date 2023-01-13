25ce0ef5beef8af960e06d55ab6bf35ebaf67f42
Akhlaque Channa <114130337+Akhlaquechannna@users.noreply.github.com>
2023-01-13 13:02:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+async function StartFunc() {
+    const systemNum = Math.floor(Math.random() * 10);
+    const userNum = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'userGuess',
+            message: "Enter your Number B/w 1 - 10: "
+        }
+    ]);
+    const { userGuess } = userNum;
+    console.log("our Guess: ", userGuess, "\nSystem's Guess: ", systemNum);
+    if (userGuess === systemNum) {
+        console.log("your Guess Is correct \nYou Won! ");
+    }
+    else {
+        console.log("Better luck next time....");
+    }
+}
+async function StarAgain() {
+    do {
+        await StartFunc();
+        var again = await inquirer
+            .prompt({
+            type: "input",
+            name: 'restart',
+            message: "Do you want to continue ? press Y or N"
+        });
+    } while (again.restart === "Y" || again.restart === "y" || again.restart === "YES" || again.restart === "yes");
+}
+StarAgain();
